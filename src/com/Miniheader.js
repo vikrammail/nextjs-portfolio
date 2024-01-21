b5cfec9f3d8d82c8436008845872005d19a233b4
vikrammail <121216954+vikrammail@users.noreply.github.com>
2024-01-21 21:54:16 +0530

    Update Miniheader.js

--- src/com/Miniheader.js
+++ src/com/Miniheader.js
@@ -12,16 +12,16 @@ export default function Miniheader() {
   const pathname = usePathname()
   return (
     <>
-    <Flex bg={''} position={'fixed'} backdropFilter={'blur(10px)'} p={4} borderWidth={1}  justifyContent="center" w="100%">
-      <Box flex="1">
+    <Flex  p={4} borderWidth={1}  justifyContent="center" w="100%">
+      <Box flex="1" display={{ base: "block", md: "none" }}>
          <DrawerExample/>
       </Box>
       <Box display={{ md: 'display' }}  ml="auto" alignItems="center" justifyContent="center" h="30px">
         <Box display="flex">
       <Button colorScheme='white' variant="outline" mr="5" size="sm"><DarkModeSwitch /></Button>
-        <Text fontSize="sm" px={2} py={1} rounded={'md'} _hover={{textDecoration: 'none',bg: useColorModeValue('gray.200', 'gray.700'),}} bg={pathname =='/'? useColorModeValue('gray.300','gray.700'):''} mr="5"><Link hover__ href={'/'}>Overview</Link></Text>
-        <Text fontSize="sm" px={2} py={1} rounded={'md'} _hover={{textDecoration: 'none',bg: useColorModeValue('gray.200', 'gray.700'),}} bg={pathname =='/about'? useColorModeValue('gray.300','gray.700'):''} mr="5"><Link href={'/about'}>About</Link></Text>
-        <Text fontSize="sm" px={2} py={1} rounded={'md'} _hover={{textDecoration: 'none',bg: useColorModeValue('gray.200', 'gray.700'),}} bg={pathname =='/exp'? useColorModeValue('gray.300','gray.700'):''} mr="5"><Link href={'/exp'}>Experience</Link></Text>
+        <Text fontSize="sm" px={2} py={1} rounded={'md'} _hover={{textDecoration: 'none',bg: useColorModeValue('gray.200', 'purple.700'),}} bg={pathname =='/'? useColorModeValue('gray.300','purple.700'):''} mr="5"><Link hover__ href={'/'}>Overview</Link></Text>
+        <Text fontSize="sm" px={2} py={1} rounded={'md'} _hover={{textDecoration: 'none',bg: useColorModeValue('gray.200', 'purple.700'),}} bg={pathname =='/about'? useColorModeValue('gray.300','purple.700'):''} mr="5"><Link href={'/about'}>About</Link></Text>
+        <Text fontSize="sm" px={2} py={1} rounded={'md'} _hover={{textDecoration: 'none',bg: useColorModeValue('gray.200', 'purple.700'),}} bg={pathname =='/exp'? useColorModeValue('gray.300','purple.700'):''} mr="5"><Link href={'/exp'}>Experience</Link></Text>
         {/* <Text fontSize="md" mr="5"><Link href={'/test'}>test</Link></Text> */}
         {/* <Text fontSize="md" mr="5">Terms</Text> */}
       </Box>
