bae50458d64e28aa4464089189c5ae1235222a71
vikrammail <121216954+vikrammail@users.noreply.github.com>
2024-01-21 21:57:54 +0530

    Update page.js

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,36 +1,36 @@
-"use client"
-import { Box, Heading, Text, VStack, Link } from '@chakra-ui/react';
-
+import React from "react";
+import {  Box, Heading, Text, Link } from "@chakra-ui/react";
 
 const AboutUs = () => {
   return (
-    <Box p="8" >
-      <Heading as="h2" mb="6" fontSize="2xl" mt={90}>
-        About Us
-      </Heading>
-
-      <VStack align="start" spacing="4">
-        <Text>
-          Hi there! I'm vikram kumar, a passionate Full Stack Developer with a love for creating web applications that are both elegant and efficient. I specialize in building end-to-end solutions, from designing the user interface to implementing server-side logic and database management.
+    <>
+      <Box maxW="600px" mx="auto" textAlign="center" mt={10} p={5}>
+        <Heading as="h2" size="xl" mb={4}>
+          About Me
+        </Heading>
+        <Text fontSize="lg">
+          Welcome to my space on the web! I'm a passionate Full Stack MERN Developer who loves turning ideas into functional and elegant web applications.
         </Text>
-
-        <Text>
-          My journey in web development began with a curiosity for creating things that live on the internet. Over the years, I've honed my skills in various technologies, allowing me to deliver high-quality software solutions for clients and users alike.
+        <Text fontSize="lg" mt={4}>
+          My expertise lies in crafting seamless and responsive user interfaces using React.js, backed by the power of Node.js and Express.js for building robust server-side applications. MongoDB is my database of choice for efficient data management.
         </Text>
-
-        <Text>
-          As a Full Stack Developer, I am proficient in both frontend and backend technologies. On the frontend, I enjoy crafting beautiful and responsive user interfaces using Chakra UI, React, and other modern JavaScript frameworks. On the backend, I'm experienced in server-side development using Node.js, Express, and database management with technologies like MongoDB and SQL.
+        <Text fontSize="lg" mt={4}>
+          I'm on a constant journey of learning and staying updated with the latest technologies and best practices in web development. My goal is to not only meet the technical requirements of a project but to exceed expectations through creativity and innovation.
         </Text>
-
-        <Text>
-          I believe in the power of clean code and maintainable architecture. My goal is to create web applications that not only meet the functional requirements but also provide an excellent user experience. I am always eager to learn and stay up-to-date with the latest technologies to enhance my skills and bring innovative solutions to the projects I work on.
+        <Text fontSize="lg" mt={4}>
+          As a collaborative team player, I've had the pleasure of working with diverse teams, bringing ideas to life and delivering high-quality software solutions.
         </Text>
-
-        <Text>
-          Let's connect! Feel free to reach out if you have a project in mind, need consultation, or just want to discuss the latest trends in web development. You can find me on <Link color="blue.500" href="[Your LinkedIn Profile]">LinkedIn</Link> or drop me an email at [your.email@example.com].
+        <Text fontSize="lg" mt={4}>
+          Let's connect and explore the endless possibilities of web development! Feel free to reach out to me on {" "}
+          <Link href="https://www.linkedin.com/in/vikram-verma-44221b254/" isExternal color="teal.500" fontWeight="bold">
+            LinkedIn
+          </Link>{" "}or{" "}
+          <Link href="https://github.com/vikrammail" isExternal color="teal.500" fontWeight="bold">
+            GitHub
+          </Link>.
         </Text>
-      </VStack>
-    </Box>
+      </Box>
+    </>
   );
 };
 
