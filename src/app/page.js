44bcd7429441ca6310476d5f9036d5b200f0e71d
vikrammail <121216954+vikrammail@users.noreply.github.com>
2024-01-21 22:00:28 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,26 @@
-import Header from '@/com/Header'
-import Port from '@/com/portfolio/Port'
+"use client"
+import { Button } from '@chakra-ui/react'
 import Image from 'next/image'
 import Link from 'next/link'
+// import { useSelector } from 'react-redux'
+// import Alluser from '@/rdcom/alluser'
+// import Setuser from '@/rdcom/setuser'
+import Hero from '@/com/portfolio/Hero'
+import Profile from '@/com/portfolio/Profile'
 
 
 export default function Home() {
+  // const state = useSelector((state)=>state.ChangeColor.color)
+  // console.log(state)
   return (
-    <main className=''> 
-      <Header/>
-    <Port/>
+    <>
+    <main > 
+      <Hero/>
+      <Profile/>
+    {/* <Setuser/> */}
+    {/* <Alluser/> */}
 
     </main>
+    </>
   )
 }
